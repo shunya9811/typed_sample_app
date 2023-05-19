635677223313735656c0e33efeb92ad77b6f08cf
shunya9811 <64852663+shunya9811@users.noreply.github.com>
2023-05-19 14:01:09 +0000

    Finish user edit, update, index, and destroy actions (10-4)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_17_074423) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_130623) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_17_074423) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
