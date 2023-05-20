547c5766bb4e3b2d4f24d30e1dfae28d5bfdc104
shunya9811 <64852663+shunya9811@users.noreply.github.com>
2023-05-20 17:09:47 +0000

    Add PasswordResets resources and ability to password reset email sent (12-1, 12-2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  #get 'sessions/new'
   root 'static_pages#home'
 
   # get 'static_pages/home'
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   delete "/logout",  to: "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
