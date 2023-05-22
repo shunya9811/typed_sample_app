0f7ee3e1d91048b41def4ea1a5ade8a578f3a463
shunya9811 <64852663+shunya9811@users.noreply.github.com>
2023-05-22 02:27:27 +0000

    Add user microposts (13-4)

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -70,3 +70,18 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "1 micropost", response.body
   end
 end
+
+class ImageUploadTest < MicropostsInterface
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type=file]'
+  end
+
+  test "should be able to attach an image" do
+    content = "This micropost really ties the room together."
+    img  = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: content, image: img } }
+    assert assigns(:micropost).image.attached?
+  end
+end
