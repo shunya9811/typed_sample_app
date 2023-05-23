6b548c8ac3cdc488201772e44a589477537ee6de
shunya9811 <64852663+shunya9811@users.noreply.github.com>
2023-05-23 02:57:49 +0000

    Add user following (14-3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,11 +97,23 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
-  # 試作feedの定義
-  # 完全な実装は次章の「ユーザーをフォローする」を参照
   # SQLインジェクション（SQL Injection）の対策のため、常にエスケープするようにする
   def feed
-    Micropost.where("user_id = ?", id)
+    # スケールが小さければこれでいい　rails側で探査しているコード
+    # Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+
+    # スケールが大きいならデータベース側で探査するべき 
+    # database側で探査しているコード
+    # following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    # Micropost.where("user_id IN (#{following_ids}) 
+    #                  OR user_id = :user_id", user_id: id)
+    #                  .includes(:user, image_attachment: :blob)
+
+    # 演習14.3.3.3
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers)
+             .where(part_of_feed, { id: id }).distinct
+             .includes(:user, image_attachment: :blob)
   end
 
   # ユーザーをフォローする
